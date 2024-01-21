5ec354cd5b50cad9d9b9fdd8d2437aefbb1f5a52
Geoff 👌 Cowan <geoffroy.cowan@gmail.com>
2024-01-21 17:14:59 +1100

    fix maths

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-  now = new Date();
-  year = now.getFullYear();
-  startOfYear = new Date(year, 0, 1);
-  daysInYear = (
-    (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)
-  ) ? 366 : 365;
-  yearMs = daysInYear * 24 * 60 * 60 * 1000;
+year = new Date().getFullYear();
+startOfYear = new Date(year, 0, 0);
+daysInYear = (
+  (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)
+) ? 366 : 365;
+yearMs = daysInYear * 24 * 60 * 60 * 1000;
 
-  update = () => {
-    now = new Date();
-    deltaMs = now - startOfYear;
-    progress = deltaMs / yearMs * 100;
-    document.body.innerText = `${progress.toFixed(7)}%`;
-  }
-  setInterval(update, 15);
+update = () => {
+  deltaMs = new Date() - startOfYear;
+  progress = deltaMs / yearMs * 100;
+  document.body.innerText = `${progress.toFixed(7)}%`;
+}
+setInterval(update, 15);
